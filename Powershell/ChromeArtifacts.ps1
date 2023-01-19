4b14043ac80b55ef01b8e8f8c06d31ba7cf93630
オークO A K Z E H オーク <71057925+Oakzeh@users.noreply.github.com>
2023-01-20 04:33:38 +1100

    Update ChromeArtifacts.ps1

--- Powershell/ChromeArtifacts.ps1
+++ Powershell/ChromeArtifacts.ps1
@@ -1,56 +1,37 @@
 ### CHROME ### 
 $chrome = Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'
 #Checking if Chrome is Installed
-
-
 mkdir "C:\Users\$env:UserName\Desktop\Artifacts" -Force
-
 if($chrome) {
 mkdir "C:\Users\$env:UserName\Desktop\Artifacts\Chrome" -Force
 #Creating Chrome Sub Folder
 
 $path = "C:\Users\$env:UserName\AppData\Local\Google\Chrome\User Data\"
-$profiles = 1
-$v = Test-Path "$path\Profile $profiles"
+$profiles = @()
 #defining variables
-
-for($i=1; $i -le 50; $i++) {
-    #Write-Output $i
-    $v = Test-Path "$path\Profile $i"
-    $profiles++
-
-      if (!$v) {
-    break
-    }
-}
-#Testing and finding chrome user profile paths 
-
 
 $Default = @("History", "Visited Links", "Web Data", "Top Sites", "Custom Dictionary.txt","Shortcuts", 
 'Bookmarks', "Preferences", "Login Data", "Media History", "Shortcuts", "Network Action Predictor", "Favicons")
 #Array of Default folder Artifatcs
 
-for($i=1; $i -le $profiles; $i++){
-
-if (Test-Path "$path\Profile $i"){
-
-mkdir "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\Profile $i" -Force}
-#Creates Sub folder for Individual Profiles
-
-foreach($artifact in $Default){
-
-if (test-path -path "$path\Profile $i\$artifact"){
-
-Copy-Item -path "$path\Default\$artifact" -destination "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\Profile $i\$artifact $i"
-Write-Output "Profile $i - $artifact Copied Successfully"
+$profiles = @(Get-ChildItem $path -Filter "Profile*")
+foreach($i in $profiles){
+  if (Test-Path "$path\$i"){
+    mkdir "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\$i" -Force
+    #Creates Sub folder for Individual Profiles
+    
+    foreach($artifact in $Default){
+    
+    if (test-path -path "$path\$i\$artifact"){
+    
+    Copy-Item -path "$path\Default\$artifact" -destination "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\$i\$artifact-$i"
+    Write-Output "$i - $artifact Copied Successfully"
+    }}
 }}
-
-
 
 mkdir "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\Default" -Force
 #Creating folder for Default
 
-}
 #Copies Cookies
 if (Test-Path "$path\Default\Network\cookies") {
 Copy-Item -path "$path\Default\Network\cookies" -destination "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\Default\cookies" #-Force
@@ -71,9 +52,13 @@ if (test-path -path "$path\Default\Cache\Cache_Data") {
 Copy-Item -path "$path\Default\Cache\Cache_Data" -Destination "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\Default\" -Recurse -Force
 Write-Output "Cache Files Copied Successfully"
 }
+
+#if (test-path -path "C:\Windows\Prefetch\CHROME.EXE*") {
+#Copy-Item -path "C:\Windows\Prefetch\CHROME.EXE*" -Destination "C:\Users\$env:UserName\Desktop\Artifacts\Chrome\Default\" -Recurse -Force
+#Write-Output "Chrome Prefetch Copied Successfully"
+#Prefetch File for chrome }
 }
 }
 
-else {
-Write-Output "Chrome not installed"
-}
+else {Write-Output "Chrome not installed"}
+
