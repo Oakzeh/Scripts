da1571b0434c7e219978f2bb950e73930b3613d6
オークO A K Z E H オーク <71057925+Oakzeh@users.noreply.github.com>
2023-01-20 17:43:13 +1100

    Update Firefox Artifacts.ps1

--- Powershell/Firefox Artifacts.ps1
+++ Powershell/Firefox Artifacts.ps1
@@ -1,4 +1,4 @@
-﻿### FIREFOX ### 
+### FIREFOX ### 
 $firefox = Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe'
 #Checking if Firefox is Installed
 
@@ -8,8 +8,8 @@ mkdir "C:\Users\$env:UserName\Desktop\Artifacts\Firefox" -Force
 
 $ffpath = "C:\Users\$env:UserName\AppData\Roaming\Mozilla\Firefox\Profiles\"
 
-$ffArtifacts = @("places.sqlite", "cookies.sqlite", "favicons.sqlite", "formhistory.sqlite", 
-'storage.sqlite', "webappsstore.sqlite", "protections.sqlite", "content-prefs.sqlite", "permissions.sqlite")
+$ffArtifacts = @("places.sqlite", "cookies.sqlite", "favicons.sqlite", "formhistory.sqlite", "key4.db", "logins.json", "persdict.dat", "xulstore.json", "prefs.js",
+'storage.sqlite', "webappsstore.sqlite", "protections.sqlite", "content-prefs.sqlite", "permissions.sqlite", "bookmarkbackups", "cert9.db", "sessionstore.jsonlz4", "storage\default")
 #Array of Default folder Artifatcs
 
 
@@ -21,10 +21,10 @@ foreach ($profile in $ffprofiles) {
     Write-Host $ffpath$profile
     foreach($art in $ffArtifacts) {
         if (test-path "$ffpath$profile\$art") {
-            Copy-Item -Path "$ffpath$profile\$art" -Destination "C:\Users\$env:UserName\Desktop\Artifacts\Firefox\$profile\" -Force
-            Write-Host "$profile $art Copied Successfully!"
-            #nested for loop checking if artifact path is valid then copying to specified folder
+            Copy-Item -Path "$ffpath$profile\$art" -Destination "C:\Users\$env:UserName\Desktop\Artifacts\Firefox\$profile\" -Recurse -Force
+            Write-Host "$profile $art Copied Successfully"
         }
+#nested for loop checking if artifact path is valid then copying to specified folder
     }
 }
 }
